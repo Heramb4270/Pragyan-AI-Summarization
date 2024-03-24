7a5eb04315853c3a89842c4ba11da13c6c049997
bhaveshxop <113847606+bhaveshxop@users.noreply.github.com>
2024-03-25 00:15:13 +0530

    some minor changes

--- frontend/components/Home/navbar.js
+++ frontend/components/Home/navbar.js
@@ -155,7 +155,7 @@ const Navbar = () => {
                 type="button"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none "
               >
-                Login/Register
+                Login
               </button>
             </Link>
           )}
